871e02e0c1412c4af7fdef02ca531af0cf6e9e81
vibhor kumar <vibhork85@gmail.com>
2024-11-06 21:15:41 +0530

    app setup

---
+++ src/main/java/com/practice/atm/ATMBank/AtmBankApplication.java
@@ -0,0 +1,13 @@
+package com.practice.atm.ATMBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtmBankApplication.class, args);
+	}
+
+}
